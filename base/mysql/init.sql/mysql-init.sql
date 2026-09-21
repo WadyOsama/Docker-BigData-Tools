51248b84bd90979bcb0602eb56efe321cb0a7565
WadyOsama <wady.alazb@gmail.com>
2026-09-21 20:48:06 +0300

    sql init fix 2

--- base/mysql/init.sql/mysql-init.sql
+++ base/mysql/init.sql/mysql-init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS hue CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'secret';
+-- root@'%' is already provisioned by MYSQL_ROOT_USER/MYSQL_ROOT_PASSWORD; re-creating it here caused ERROR 1396 on init
 GRANT ALL PRIVILEGES ON hue.* TO 'root'@'%';
 FLUSH PRIVILEGES;
